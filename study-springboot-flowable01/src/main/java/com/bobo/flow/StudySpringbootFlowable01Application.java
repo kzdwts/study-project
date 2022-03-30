e55cd51acf8bbba873a6c68ef072d0dc3bf521a9
kangyong <kangyong_wts@sina.com>
2022-03-30 22:58:34 +0800

    feat: 初始化flowable demo工程

---
+++ study-springboot-flowable01/src/main/java/com/bobo/flow/StudySpringbootFlowable01Application.java
@@ -0,0 +1,13 @@
+package com.bobo.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringbootFlowable01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringbootFlowable01Application.class, args);
+    }
+
+}
