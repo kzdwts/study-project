bb65a08ceb66b4b32fefc6d0b55c9649ff5a8074
kangyong <kangyong_wts@sina.com>
2022-04-01 17:55:07 +0800

    feat: 初始化ProcessEngine实例

---
+++ study-flowable-demo01/src/test/java/com/bobo/flowable/test/ProcessEngineTest.java
@@ -0,0 +1,35 @@
+package com.bobo.flowable.test;
+
+
+import org.flowable.engine.ProcessEngine;
+import org.flowable.engine.ProcessEngineConfiguration;
+import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.junit.Test;
+
+/**
+ * test
+ *
+ * @author Kang Yong
+ * @date 2022/4/1
+ * @since 1.0.0
+ */
+public class ProcessEngineTest {
+
+    /**
+     * 初始化**ProcessEngine**流程引擎实例
+     *
+     * @author Kang Yong
+     * @date 2022/4/1
+     */
+    @Test
+    public void processEngine01() {
+        ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
+                .setJdbcUrl("jdbc:mysql://192.168.100.134:3306/flowable-learn01?serverTimezone=UTC")
+                .setJdbcUsername("root")
+                .setJdbcPassword("1024KangYong@MySQL")
+                .setJdbcDriver("com.mysql.cj.jdbc.Driver")
+                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+
+        ProcessEngine processEngine = cfg.buildProcessEngine();
+    }
+}
