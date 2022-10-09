b571a273b7e436e3c6ba625c91d43d49c58e5ce6
kangyong <kangyong_wts@sina.com>
2022-10-09 16:31:54 +0800

    feat: 循环依赖

---
+++ study-springboot-dep/src/main/java/top/kangyong/springboot/dep/DepApplication.java
@@ -0,0 +1,21 @@
+package top.kangyong.springboot.dep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author Kang Yong
+ * @date 2022/10/9
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class DepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepApplication.class, args);
+    }
+
+}
+
