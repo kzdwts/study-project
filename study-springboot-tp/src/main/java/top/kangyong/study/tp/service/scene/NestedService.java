813a84b13ab0d4234b09655f485d1ecae648ede9
kangyong <kangyong_wts@sina.com>
2023-02-19 23:04:12 +0800

    feat: 验证Propagation.NESTED

--- study-springboot-tp/src/main/java/top/kangyong/study/tp/service/scene/NestedService.java
+++ study-springboot-tp/src/main/java/top/kangyong/study/tp/service/scene/NestedService.java
@@ -8,4 +8,21 @@ package top.kangyong.study.tp.service.scene;
  * @since 1.0.0
  */
 public interface NestedService {
+
+//    场景一：此场景外围方法没有开启事务
+
+    void notransaction_exception_nested_nested();
+
+    void notransaction_nested_nested_exception();
+
+//  场景二：外围方法开启事务
+
+    void transaction_exception_nested_nested();
+
+
+    void transaction_nested_nested_exception();
+
+
+    void transaction_nested_nested_exception_try();
+
 }
--- study-springboot-tp/src/main/java/top/kangyong/study/tp/service/scene/impl/NestedServiceImpl.java
+++ study-springboot-tp/src/main/java/top/kangyong/study/tp/service/scene/impl/NestedServiceImpl.java
@@ -1,6 +1,10 @@
 package top.kangyong.study.tp.service.scene.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import top.kangyong.study.tp.service.scene.NestedService;
+import top.kangyong.study.tp.service.user.User1Service;
+import top.kangyong.study.tp.service.user.User2Service;
 
 /**
  * nested
@@ -9,5 +13,40 @@ import top.kangyong.study.tp.service.scene.NestedService;
  * @date 2023/2/16
  * @since 1.0.0
  */
+@Service
 public class NestedServiceImpl implements NestedService {
+
+    @Autowired
+    private User1Service user1Service;
+
+    @Autowired
+    private User2Service user2Service;
+
+    @Override
+    public void notransaction_exception_nested_nested() {
+
+    }
+
+    @Override
+    public void notransaction_nested_nested_exception() {
+
+    }
+
+//    场景二：外围方法开启事务
+
+
+    @Override
+    public void transaction_exception_nested_nested() {
+
+    }
+
+    @Override
+    public void transaction_nested_nested_exception() {
+
+    }
+
+    @Override
+    public void transaction_nested_nested_exception_try() {
+
+    }
 }
