d7b39a638663e49e804bf62649dabc761b7525f2
kangyong <kangyong_wts@sina.com>
2023-02-20 22:15:34 +0800

    feat: 验证Propagation.NESTED

--- study-springboot-tp/src/main/java/top/kangyong/study/tp/service/scene/impl/NestedServiceImpl.java
+++ study-springboot-tp/src/main/java/top/kangyong/study/tp/service/scene/impl/NestedServiceImpl.java
@@ -11,6 +11,9 @@ import top.kangyong.study.tp.service.user.User2Service;
 
 /**
  * nested
+ * <p>
+ * 如果当前存在事务，则在嵌套事务内执行。
+ * 如果当前没有事务，则执行与Propagation.REQUIRED类似的操作
  *
  * @author Kang Yong
  * @date 2023/2/16
@@ -26,7 +29,20 @@ public class NestedServiceImpl implements NestedService {
     private User2Service user2Service;
 
 //    场景一：此场景外围方法没有开启事务
+//    通过这两个方法我们证明了在外围方法未开启事务的情况下Propagation.NESTED和Propagation.REQUIRED作用相同，
+//    修饰的内部方法都会新开启自己的事务，且开启的事务相互独立，互不干扰
 
+    /**
+     * 验证方法一
+     * <p>
+     * 数据库结果：“张三”，“李四”均插入
+     * <p>
+     * 结果分析：外围方法未开启事务，插入“张三”、“李四”方法在自己的事务中独立运行，
+     * 外围方法异常不影响内部插入“张三“、”李四“方法的独立的事务
+     *
+     * @author Kang Yong
+     * @date 2023/2/20
+     */
     @Override
     public void notransaction_exception_nested_nested() {
         User1 user1 = new User1();
@@ -40,6 +56,14 @@ public class NestedServiceImpl implements NestedService {
         throw new RuntimeException();
     }
 
+    /**
+     * 验证方法二
+     * <p>
+     * 数据库结果：“张三”插入，“李四”未插入
+     * <p>
+     * 结果分析：外围方法未开启事务，插入“张三”、“李四”方法在自己的事务中独立运行，
+     * 所以插入”李四“方法抛出异常只会回滚插入”李四“方法插入“张三“方法不受影响
+     */
     @Override
     public void notransaction_nested_nested_exception() {
         User1 user1 = new User1();
@@ -53,7 +77,14 @@ public class NestedServiceImpl implements NestedService {
 
 //    场景二：外围方法开启事务
 
-    @Transactional // 默认spring事务传播行为 propagation = Propagation.NESTED
+    /**
+     * 验证方法一
+     * <p>
+     * 数据库结果：“张三”,“李四”未均插入
+     * <p>
+     * 结果分析：外围方法开启事务，内部事务为外围事务的子事务，外围方法回滚，内部方法也要回滚
+     */
+    @Transactional // 默认spring事务传播行为 propagation = Propagation.REQUIRED
     @Override
     public void transaction_exception_nested_nested() {
         User1 user1 = new User1();
@@ -67,6 +98,14 @@ public class NestedServiceImpl implements NestedService {
         throw new RuntimeException();
     }
 
+    /**
+     * 验证方法二
+     * <p>
+     * 数据库结果：“张三”,“李四”未均插入
+     * <p>
+     * 结果分析：外围方法开启事务，内部事务为外围事务的子事务，内部方法抛出异常回滚，
+     * 且外围方法感知异常致使整体事务回滚
+     */
     @Transactional
     @Override
     public void transaction_nested_nested_exception() {
@@ -79,6 +118,14 @@ public class NestedServiceImpl implements NestedService {
         user2Service.addNestedException(user2);
     }
 
+    /**
+     * 验证方法三
+     * <p>
+     * 数据库结果：“张三”插入,“李四”未插入
+     * <p>
+     * 结果分析：外围方法开启事务，内部事务为外围事务的子事务，
+     * 插入“李四”内部方法抛出异常，可以单独对子事务回滚
+     */
     @Transactional
     @Override
     public void transaction_nested_nested_exception_try() {
