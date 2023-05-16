f17f7f234254e3df4a2c883fddb2c0f8d2f57db3
kangyong <kangyong_wts@sina.com>
2023-05-16 11:44:54 +0800

    feat: spring重试机制 retry

---
+++ study-springboot-retry/src/main/java/top/kangyong/retry/ServerRetryApplication.java
@@ -0,0 +1,20 @@
+package top.kangyong.retry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * start
+ *
+ * @author Kang Yong
+ * @date 2023/5/18
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class ServerRetryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerRetryApplication.class, args);
+    }
+
+}
