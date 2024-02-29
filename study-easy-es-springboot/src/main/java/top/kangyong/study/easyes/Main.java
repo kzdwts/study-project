cd5ed85a8105967addbbef40072b2ff058876576
kangyong <kangyong_wts@sina.com>
2024-02-29 10:46:23 +0800

    feat: 新增ee项目

---
+++ study-easy-es-springboot/src/main/java/top/kangyong/study/easyes/Main.java
@@ -0,0 +1,14 @@
+package top.kangyong.study.easyes;
+
+/**
+ * TODO description .
+ *
+ * @author Kang Yong
+ * @date 2024/2/29
+ * @since 1.0.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
